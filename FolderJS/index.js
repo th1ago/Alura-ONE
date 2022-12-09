f8754514a06ae8f7a9733a9b79eee637433597f5
Thiago <thiago_murakami@hotmail.com>
2022-12-09 17:32:44 -0300

    work with canvas

---
+++ FolderJS/index.js
@@ -0,0 +1,7 @@
+var tela = document.querySelector("canvas")
+var pincel = tela.getContext("2d")
+
+//  uma propriedade
+pincel.fillStyle = "lightgrey"
+// uma function
+pincel.fillRect(0, 0, 600, 400)
