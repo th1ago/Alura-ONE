de44cec8a43e3606ea1436c915267116910303c6
Thiago <thiago_murakami@hotmail.com>
2022-12-22 15:24:47 -0300

    adding style

--- Challenge_ONE_Sprint_01/script.js
+++ Challenge_ONE_Sprint_01/script.js
@@ -1,10 +1,24 @@
 document.querySelector('button').addEventListener('click', criptografa);
 document.querySelector('button').addEventListener('click', descriptografa);
 
-function criptografa() {
-    var value = '2';
-    document.getElementById('cripto').innerHTML = value;
+function criptografa(){
+
+    var mensagem = document.dados.msg.value;
+    var ascii = 0;
+    x=0;
+
+    console.log(mensagem)
+
+    while(mensagem[x]) {
+        ascii = mensagem[x].charCodeAt();
+        ascii+=3;
+
+    document.dados.mensagem2.value += String.fromCharCode(ascii);
+    x++;
+
+    }
 }
+
 
 function descriptografa(){
 
