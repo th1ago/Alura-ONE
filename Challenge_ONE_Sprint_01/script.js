0b3a4878b172ddd798d76268239a3eaa48ff7190
Thiago <thiago_murakami@hotmail.com>
2022-12-22 15:10:36 -0300

    add file

---
+++ Challenge_ONE_Sprint_01/script.js
@@ -0,0 +1,11 @@
+document.querySelector('button').addEventListener('click', criptografa);
+document.querySelector('button').addEventListener('click', descriptografa);
+
+function criptografa() {
+    var value = '2';
+    document.getElementById('cripto').innerHTML = value;
+}
+
+function descriptografa(){
+
+}
