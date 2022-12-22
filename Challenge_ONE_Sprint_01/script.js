7041a0fe8e502a32fdf729b2dfc3b003517cce09
Thiago <thiago_murakami@hotmail.com>
2022-12-22 16:48:50 -0300

    deploy logic

--- Challenge_ONE_Sprint_01/script.js
+++ Challenge_ONE_Sprint_01/script.js
@@ -1,25 +1,11 @@
-document.querySelector('button').addEventListener('click', criptografa);
-document.querySelector('button').addEventListener('click', descriptografa);
+var textInput = document.querySelector('button').addEventListener('click', criptografa);
+var outInput = document.querySelector('button').addEventListener('click', descriptografa);
 
-function criptografa(){
-
-    var mensagem = document.dados.msg.value;
-    var ascii = 0;
-    x=0;
-
-    console.log(mensagem)
-
-    while(mensagem[x]) {
-        ascii = mensagem[x].charCodeAt();
-        ascii+=3;
-
-    document.dados.mensagem2.value += String.fromCharCode(ascii);
-    x++;
-
-    }
+function criptografa() {
+    var texto = textInput.value;
+    
 }
 
-
 function descriptografa(){
-
+    
 }
