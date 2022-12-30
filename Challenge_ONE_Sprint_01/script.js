47e21052ecdadf23e304eb0cfa14fd56bf0bdd8e
Thiago <thiago_murakami@hotmail.com>
2022-12-29 23:04:06 -0300

    work in js

--- Challenge_ONE_Sprint_01/script.js
+++ Challenge_ONE_Sprint_01/script.js
@@ -1,9 +1,23 @@
-function criptografa() {
-    let textarea = document.getElementById("input");
-    textarea.select
+var textArea = document.querySelector("#mensagem");
 
+function criptografa() {
+    var crypto = textArea.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
+
+    document.getElementById('card').innerHTML = '<textarea readonly id="output-texto">' + crypto + 
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+
+    console.log(crypto);
 }
 
 function descriptografa(){
     
+}
+
+function copiar() {
+    var copia = document.getElementById("output-texto");
+
+    copia.select();
+    document.execCommand("copy");
+
+    console.log(copia);
 }
