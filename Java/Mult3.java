cb5c9636089b55306fd751fe86bbb8676f9c0002
Thiago <thiago_murakami@hotmail.com>
2023-02-28 15:19:38 -0300

    add files

---
+++ Java/Mult3.java
@@ -0,0 +1,5 @@
+// Utilize um laço do tipo for para imprimir todos os múltiplos de 3, entre 1 e 100.
+
+public class Mult3 {
+    
+}
---
+++ Java/fatorial.java
@@ -0,0 +1,6 @@
+// Agora crie uma nova classe, escreva um for que inicie uma variável n (número atual) como 1 e fatorial
+// (resultado total) como 1. Faça seu laço entre 1 a 10 e calcule o resultado!
+
+public class fatorial {
+    
+}
