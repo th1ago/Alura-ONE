77db98bd3f5eef2de87f6089820a7b89cca7d374
Thiago <thiago_murakami@hotmail.com>
2023-03-02 20:08:50 -0300

    work in the input dialog

--- Java/JavaSprint01.java
+++ Java/JavaSprint01.java
@@ -2,11 +2,18 @@ import javax.swing.JOptionPane;
 
 public class JavaSprint01 {
     public static void main(String[] args) {
-        Object[] possibleValues = {"Moeda, Temperatura"};
-        JOptionPane.showInputDialog(null,
-        "Selecione um conversor", 
-        "Conversor",
-        JOptionPane.INFORMATION_MESSAGE, null,
-        possibleValues, possibleValues[0]);
+        Object[] possibleValues = {"Moeda", "Temperatura"};
+        Object conversor = JOptionPane.showInputDialog(null,
+                "Selecione um conversor", 
+                "Conversor",
+                JOptionPane.INFORMATION_MESSAGE,
+                null,
+                possibleValues, possibleValues[0]);
+
+        if (conversor == possibleValues[0]) {
+            System.out.println("Qual a moeda selecionada");
+        } else {
+            System.out.println("Selecione a temperatura");
+        }
     }   
 }
