f9baf6564e47bcf42cb3cbddb90b40d229cfb399
Thiago <thiago_murakami@hotmail.com>
2023-03-02 20:14:33 -0300

    create conversor C to F

--- Java/JavaSprint01.java
+++ Java/JavaSprint01.java
@@ -1,6 +1,12 @@
 import javax.swing.JOptionPane;
 
 public class JavaSprint01 {
+
+    // (0 °C × 9/5) + 32 = 32 °F
+    static double conversorCelsiusFahrenheit(double c) {
+        return 9 * c / 5 + 32;
+    }
+
     public static void main(String[] args) {
         Object[] possibleValues = {"Moeda", "Temperatura"};
         Object conversor = JOptionPane.showInputDialog(null,
