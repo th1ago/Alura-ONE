36fd7eab98b1da9c807318e84d533908105d92f5
Thiago <thiago_murakami@hotmail.com>
2023-03-02 16:11:33 -0300

    Orientacao Obj

--- Java/CriaConta.java
+++ Java/CriaConta.java
@@ -1,5 +1,7 @@
 public class CriaConta {
     public static void main(String[] args) {
-        new Conta();
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 200;
+        System.out.println(primeiraConta.saldo);
     }   
 }
