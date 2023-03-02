7102173551f6df03e8d4498e52b8a3f21ba0b812
Thiago <thiago_murakami@hotmail.com>
2023-03-02 19:29:35 -0300

    default value

--- Java/CriaConta.java
+++ Java/CriaConta.java
@@ -3,5 +3,9 @@ public class CriaConta {
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200;
         System.out.println(primeiraConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 50;
+        System.out.println(segundaConta.saldo);
     }   
 }
