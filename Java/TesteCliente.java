55cf7f32e083a9511b86389fd481972f5690ca25
Thiago <thiago_murakami@hotmail.com>
2023-03-03 13:00:53 -0300

    add new method Cliente

--- Java/TesteCliente.java
+++ Java/TesteCliente.java
@@ -1,6 +1,11 @@
 public class TesteCliente {
-    Cliente thiago = new Cliente();
-    thiago.nome = "Thiago";
-    thiago.cpf = "111.111.111-11";
-    thiago.profissao = "Analista de dados";
+    public static void main(String[] args) {
+        Cliente thiago = new Cliente();
+        thiago. nome = "Thiago";
+        thiago.cpf = "111.111.111-11";
+        thiago.profissao = "Analista de dados";
+
+        
+    }
+    
 }
