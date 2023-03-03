c5147e46a0a9f39218fbd0d650c5b30ec7e47951
Thiago <thiago_murakami@hotmail.com>
2023-03-03 11:07:12 -0300

    fix some method

--- Java/Conta.java
+++ Java/Conta.java
@@ -5,12 +5,12 @@ public class Conta {
     String titular;
 
     void deposita(float valor){
-        this.saldo += this.saldo + valor;
+        this.saldo += valor;
     }
 
     boolean saca(float valor){
         if(this.saldo >= valor){
-            this.saldo = this.saldo - valor;
+            this.saldo -= valor;
             return true;
         } else {
             return false;
