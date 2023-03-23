18be80c993bfd34285fa5ac94fd6a8c15b600d18
Thiago <thiago_murakami@hotmail.com>
2023-03-23 11:40:54 -0300

    add patch

--- Java/Polimorfismo/TesteGerente.java
+++ Java/Polimorfismo/TesteGerente.java
@@ -2,5 +2,12 @@ public class TesteGerente {
     
     public static void main(String[] args) {
         Gerente g1 = new Gerente();
+        g1.setNome("Thiago");
+
+        System.out.println(g1.getNome());
+
+        g1.setSenha(2222);
+        boolean autentica = g1.autentica(2222);
+        System.out.println(autentica);
     }
 }
